cae21fa029f5799f271433a61cbe02460e656774
J <stbarnes@stanford.edu>
2019-12-14 06:37:25 -0800

    Make ';' be interpreted as ':'

--- new-vim.js
+++ new-vim.js
@@ -114,7 +114,7 @@
     { keys: 'F<character>', type: 'motion', motion: 'moveToCharacter', motionArgs: { forward: false }},
     { keys: 't<character>', type: 'motion', motion: 'moveTillCharacter', motionArgs: { forward: true, inclusive: true }},
     { keys: 'T<character>', type: 'motion', motion: 'moveTillCharacter', motionArgs: { forward: false }},
-    { keys: ';', type: 'motion', motion: 'repeatLastCharacterSearch', motionArgs: { forward: true }},
+    // { keys: ';', type: 'motion', motion: 'repeatLastCharacterSearch', motionArgs: { forward: true }},
     { keys: ',', type: 'motion', motion: 'repeatLastCharacterSearch', motionArgs: { forward: false }},
     { keys: '\'<character>', type: 'motion', motion: 'goToMark', motionArgs: {toJumplist: true, linewise: true}},
     { keys: '`<character>', type: 'motion', motion: 'goToMark', motionArgs: {toJumplist: true}},
@@ -1188,6 +1188,7 @@
             break;
           case 'ex':
           case 'keyToEx':
+            if (command.keys === ';') { command.keys = ':' }
             this.processEx(cm, vim, command);
             break;
           default:
